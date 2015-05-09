a4b84ec7470dacd7bf511cff2a7bed175695b4e8
Anton Stoychev <antitoxic@gmail.com>
2015-05-09 19:25:49 +0300

    Added icons as svgs

--- elements/select-dropdown/index.js
+++ elements/select-dropdown/index.js
@@ -31,7 +31,7 @@ module.exports = function (componentService) {
                 if (el.hasAttribute('multiple')) {
                     options.plugins = {
                         remove_button: {
-                            label: require('./close.svg')
+                            label: require('times-circle.svg')
                         }
                     }
                 }
