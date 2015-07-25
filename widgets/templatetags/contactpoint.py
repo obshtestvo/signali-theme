25aa48dd3911f9dba3179c0f3cfc07e2f037d821
Anton Stoychev <antitoxic@gmail.com>
2015-07-25 18:47:37 +0300

    Initial work for making feature homepage areas dynamic

--- widgets/templatetags/contactpoint.py
+++ widgets/templatetags/contactpoint.py
@@ -1,7 +1,10 @@
 from django import template
-from contact.models import Organisation
-from location.models import Area
+from django.db.models import Prefetch
+
 from contact.apps import setting
+from contact.models import Organisation
+from signali.models import ContactPointProxy, CategoryProxy
+from location.models import Area
 
 register = template.Library()
 
@@ -11,3 +14,23 @@ def contactpoint_stats():
         "organisations_count": Organisation.objects.all().count(),
         "areas_count": Area.objects.count_size(exclude=setting('contact_address_areasize')),
     }
+
+
+@register.inclusion_tag('_featured_areas.html')
+def featured_areas(request):
+    return {
+        "locations": Area.objects.prefetch_related(
+            Prefetch('contact_points',
+                     queryset=ContactPointProxy.objects.featured()[:5])
+        ),
+        "request": request
+    }
+
+
+@register.inclusion_tag('_featured_category.html')
+def featured_category(request):
+    return {
+        "category": CategoryProxy.objects.featured()[0],
+        "request": request
+    }
+
--- widgets/templatetags/taxonomy.py
+++ widgets/templatetags/taxonomy.py
@@ -13,8 +13,8 @@ def categories_menu(context):
         "request": context.get('request')
     }
 
-@register.inclusion_tag('taxonomy/_picker.html')
-def taxonomy_picker(request, form=None):
+@register.inclusion_tag('taxonomy/_mixed_picker_options.html')
+def taxonomy_mixed_options(request, form=None):
     if form is None:
         form = UserCriteria(data=request.params.copy())
 
