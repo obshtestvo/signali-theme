5b3f7a37faf702568ed40c9e34690b017a70de38
Boyan Todorov <todorov.boyan@gmail.com>
2015-08-24 00:18:13 +0300

    added ugly icon for list-item type none (X)

--- elements/list-item/index.js
+++ elements/list-item/index.js
@@ -4,7 +4,20 @@ module.exports = function (componentService) {
     componentService.register('list-item', {
         template: require('./list-item.html'),
         include: {
-            checkCircle: require('./check-circle.svg')
+            checkCircle: require('./check-circle.svg'),
+            closeCircle: require('./close.svg')
+        },
+        properties: {
+            "check": {
+                get: function () {
+                    return this.getAttribute('type')=='check'
+                }
+            },
+            "none": {
+                get: function () {
+                    return this.getAttribute('type')=='none'
+                }
+            }
         }
     })
 }
