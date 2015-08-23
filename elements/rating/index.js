69beba3d1d3832aa7b91364efb574c3e1b4817b4
Boyan Todorov <todorov.boyan@gmail.com>
2015-08-23 18:54:04 +0300

    refactor rating

--- elements/rating/index.js
+++ elements/rating/index.js
@@ -5,19 +5,19 @@ module.exports = function (componentService) {
     if (componentService.has(name)) return;
     componentService.register(name, {
         template: require('./rating.html'),
-        created: function() {
-            var $el = $(this);
-            var $stars = $el.find('star');
-            $stars.each(function() {
-                var $s = $(this)
-                var $hoverStars = $s.prevAll().add($s).find('li')
-                console.log($hoverStars)
-                $s.hover(function(){
-                    $hoverStars.addClass('hover')
-                }, function(){
-                    $hoverStars.removeClass('hover')
-                })
-            })
-        }
+        //created: function() {
+        //    var $el = $(this);
+        //    var $stars = $el.find('star');
+        //    $stars.each(function() {
+        //        var $s = $(this)
+        //        var $hoverStars = $s.prevAll().add($s).find('li')
+        //        console.log($hoverStars)
+        //        $s.hover(function(){
+        //            $hoverStars.addClass('hover')
+        //        }, function(){
+        //            $hoverStars.removeClass('hover')
+        //        })
+        //    })
+        //}
     })
 }
