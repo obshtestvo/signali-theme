c45d576b8cc7bb32047033280fa4027e0df95f83
Anton Stoychev <antitoxic@gmail.com>
2015-08-23 18:28:54 +0300

    Fixes all around!

--- elements/rating/index.js
+++ elements/rating/index.js
@@ -3,8 +3,21 @@ require('./rating.scss')
 module.exports = function (componentService) {
     var name = 'rating';
     if (componentService.has(name)) return;
-
     componentService.register(name, {
-        template: require('./rating.html')
+        template: require('./rating.html'),
+        created: function() {
+            var $el = $(this);
+            var $stars = $el.find('star');
+            $stars.each(function() {
+                var $s = $(this)
+                var $hoverStars = $s.prevAll().add($s).find('li')
+                console.log($hoverStars)
+                $s.hover(function(){
+                    $hoverStars.addClass('hover')
+                }, function(){
+                    $hoverStars.removeClass('hover')
+                })
+            })
+        }
     })
 }
--- elements/survey-question/index.js
+++ elements/survey-question/index.js
@@ -2,6 +2,24 @@ require('./survey-question.scss')
 
 module.exports = function (componentService) {
     componentService.register('survey-question', {
-        template: require('./survey-question.html')
+        template: require('./survey-question.html'),
+        created: function() {
+            if (this.hasAttribute('checkbox')) {
+                var $el = $(this);
+                var $input = $el.find('input');
+                var $true = $el.find('[true]');
+                var $false = $el.find('[false]');
+                $false.click(function() {
+                    $true.attr('fill', 'outlined')
+                    $false.attr('fill', 'solid')
+                    $input.val(false);
+                });
+                $true.click(function() {
+                    $false.attr('fill', 'outlined')
+                    $true.attr('fill', 'solid')
+                    $input.val(true);
+                });
+            }
+        }
     })
 }
