ba17ff75ad93e1a085ccdf37344d363933f4c0d1
Anton Stoychev <antitoxic@gmail.com>
2015-08-23 20:08:11 +0300

    Rating now uses value instead of nested stars

--- elements/rating/index.js
+++ elements/rating/index.js
@@ -5,19 +5,44 @@ module.exports = function (componentService) {
     if (componentService.has(name)) return;
     componentService.register(name, {
         template: require('./rating.html'),
-        //created: function() {
-        //    var $el = $(this);
-        //    var $stars = $el.find('star');
-        //    $stars.each(function() {
-        //        var $s = $(this)
-        //        var $hoverStars = $s.prevAll().add($s).find('li')
-        //        console.log($hoverStars)
-        //        $s.hover(function(){
-        //            $hoverStars.addClass('hover')
-        //        }, function(){
-        //            $hoverStars.removeClass('hover')
-        //        })
-        //    })
-        //}
+        created: function() {
+            var $el = $(this);
+            var $stars = $el.find('star');
+            $stars.each(function() {
+                var $s = $(this)
+                var $hoverStars = $s.prevAll().add($s)
+                $s.hover(function(){
+                    $hoverStars.addClass('hover')
+                }, function(){
+                    $hoverStars.removeClass('hover')
+                })
+            })
+        },
+        properties: {
+            "value": {
+                get: function() {
+                    var starTypes = [];
+                    var rating = parseFloat(this.getAttribute('value'))
+                    var afterDecimalPoint = rating % 1;
+                    for (var i=1; i<=5;i++) {
+                        if (i <= rating) {
+                            starTypes.push('full');
+                            continue;
+                        }
+                        if (afterDecimalPoint) {
+                            if (afterDecimalPoint > 0.6) {
+                                starTypes.push('full');
+                            } else if (afterDecimalPoint > 0.35) {
+                                starTypes.push('half');
+                            }
+                            afterDecimalPoint = false;
+                            continue;
+                        }
+                        starTypes.push('empty');
+                    }
+                    return starTypes
+                }
+            }
+        }
     })
 }
