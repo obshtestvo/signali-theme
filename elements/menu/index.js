ad3a68b2ee54237ac1f18234eae5d9376da5b487
Boyan Todorov <todorov.boyan@gmail.com>
2015-08-25 21:03:00 +0300

    main menu refactored with new design

--- elements/menu/index.js
+++ elements/menu/index.js
@@ -1,13 +1,25 @@
-require('./menu.scss')
+var $ = require('jquery');
+require('./menu.scss');
 
 module.exports = function (componentService) {
     componentService.register('menu', {
         template: require('./menu.html'),
         created: function () {
-            $(this).find('.trigger').click(function(){
+            var $el = $(this)
+            $el.find('.trigger').click(function(){
                 $(this).toggleClass('active')
                 $(".categories").toggleClass('active')
             })
+
+            var $lists = $el.find('.categories ul');
+
+            var heights = $lists.map(function() {
+                console.log($(this).height())
+                return $(this).height()
+            }).get();
+
+            var maxHeight = Math.max.apply(null, heights);
+            $lists.height(maxHeight);
         }
     })
 }
