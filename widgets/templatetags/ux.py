3e0834562bd832407101edcc4210a9710afd1cc9
Anton Stoychev <antitoxic@gmail.com>
2015-09-01 11:49:39 +0300

    Successful registraion pjax response

---
+++ widgets/templatetags/ux.py
@@ -0,0 +1,37 @@
+from django import template
+
+register = template.Library()
+
+known_email_providers = {
+    "gmail.com": "GMail",
+    "googlemail.com": "GMail",
+    "abv.bg": "АБВ",
+    "yahoo.com": "Yahoo!",
+    "yahoo.co.uk": "Yahoo!",
+    "hotmail.com": "Outlook",
+}
+
+known_email_providers_urls = {
+    "yahoo.com": "login.yahoo.com",
+    "yahoo.co.uk": "uk.mail.yahoo.com",
+}
+
+@register.filter
+def email_provider(email):
+    provider = email.rsplit('@', 1)[1]
+    try:
+        provider = known_email_providers[provider]
+    except:
+        provider = provider.capitalize()
+
+    return provider
+
+@register.filter
+def email_provider_url(email):
+    provider = email.rsplit('@', 1)[1]
+    try:
+        provider = known_email_providers_urls[provider]
+    except:
+        pass
+
+    return "https://"+provider
