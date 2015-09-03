942e34fbc5f5163b2c087958eaa02b9f731b4665
Anton Stoychev <antitoxic@gmail.com>
2015-09-04 00:14:12 +0300

    It's late. Just more dev work.

--- elements/proposal/index.js
+++ elements/proposal/index.js
@@ -3,6 +3,15 @@ var ValidationForm = require('validation/form');
 var AjaxForm = require('ajax/form');
 require('./proposal.scss');
 
+function adjustAjaxContainers(el) {
+    var proposalContainer = $(el).closest('[proposal-container]')[0];
+    if (!proposalContainer) return;
+    var $interactionContainer = $(proposalContainer.querySelector('[animation-container]'));
+    var $replaceableElement =$interactionContainer.find('[content]').eq(0);
+    el.ajaxForm.setInteractionContainer($interactionContainer)
+    el.ajaxForm.setReplaceableElement($replaceableElement)
+}
+
 module.exports = function (componentService) {
     componentService.register('proposal', {
         template: require('./proposal.html'),
@@ -10,16 +19,22 @@ module.exports = function (componentService) {
             var el = this;
             var $el = $(el);
             var $form = $el.find('form');
+            $(el).closest('[proposal-container]').on('modal:open', function(){
+               $form.find('input:visible').eq(0).focus()
+            });
 
             var validation = new ValidationForm($form);
-            var ajaxForm = new AjaxForm($form, {
-                pjax: true,
-                interactionContainer: $form.closest('[proposal-container]')
+            el.ajaxForm = new AjaxForm($form, {
+                pjax: true
             });
             validation.on('form:submit', function() {
-                ajaxForm.submit()
+                el.ajaxForm.submit()
                 return false;
             });
+        },
+        attached: function() {
+            if (!this.ajaxForm) return;
+            adjustAjaxContainers(this);
         }
     })
 }
