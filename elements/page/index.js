b0e3e79872300208a8f401dbd41c183b44f18ac0
Anton Stoychev <antitoxic@gmail.com>
2015-09-08 00:02:39 +0300

    Made page transitions even smoother

--- elements/page/index.js
+++ elements/page/index.js
@@ -3,10 +3,17 @@ require('./page.scss')
 module.exports = function (componentService) {
     componentService.register('page', {
         created: function() {
+            var el = this;
             var loader = document.querySelector('loader[target="page"]');
+            var originalLoaderParent = loader.parentNode;
             setTimeout(function(){
                 loader.parentNode.removeChild(loader)
             }, 350);
+            window.onbeforeunload = function() {
+                console.log('aaa')
+                originalLoaderParent.appendChild(loader);
+                el.removeAttribute('resolved')
+            }
         }
     })
-}
+};
