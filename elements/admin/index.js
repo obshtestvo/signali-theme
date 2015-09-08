0d791a2117c032d07f916c9297664567cb618843
Anton Stoychev <antitoxic@gmail.com>
2015-09-08 19:49:07 +0300

    Admin jquery version workaround

--- elements/admin/index.js
+++ elements/admin/index.js
@@ -3,8 +3,12 @@
 //require('select2/dist/css/select2.css');
 //require('select2/dist/js/i18n/bg.js');
 
-window.jQuery = django.jQuery.noConflict(true);
-window.$ = window.jQuery
+// bug with jquery versions for select element
+var isEditPage = Boolean($('body.change-form').length)
+if (isEditPage) {
+    window.jQuery = django.jQuery.noConflict(true);
+    window.$ = window.jQuery
+}
 
 require('Select2/select2.css');
 require('./admin.scss');
