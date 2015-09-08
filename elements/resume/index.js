1784bae6b49f9487a9b06e4c4196f8f37cb95407
Boyan Todorov <todorov.boyan@gmail.com>
2015-09-09 00:39:29 +0300

    footer-fix

--- elements/resume/index.js
+++ elements/resume/index.js
@@ -2,6 +2,9 @@ require('./resume.scss')
 
 module.exports = function (componentService) {
     componentService.register('resume', {
-        template: require('./resume.html')
+        template: require('./resume.html'),
+        include: {
+            tagsIcon: require('./icon-tags.svg')
+        }
     })
 }
