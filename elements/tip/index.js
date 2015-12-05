10d4b70bbea7404a0c9c6b258027068ebeb132e9
Boyan Todorov <todorov.boyan@gmail.com>
2015-12-05 20:30:43 +0200

    Не трябва да има ID-та в template-ите на custom елементите #2

--- elements/tip/index.js
+++ elements/tip/index.js
@@ -5,14 +5,6 @@ module.exports = function (componentService) {
         template: require('./tip.html'),
         include: {
             tip: require('./tip.svg')
-        },
-        type: {
-            attr: true,
-            set: function(value) {
-                $(this).find('tip').attr('type', value);
-            }
         }
-    });
-    componentService.register('tip-text', {
     })
 }
