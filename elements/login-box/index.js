2106bc38d488d7a4c5c8884ad02e08b5825a4e6a
Anton Stoychev <antitoxic@gmail.com>
2016-01-07 21:23:31 +0200

    Detect touch devices for login box

--- elements/login-box/index.js
+++ elements/login-box/index.js
@@ -2,8 +2,24 @@ import './login-box.scss'
 import template from './login-box.html'
 import iconUser from './user.svg'
 
+
+function is_touch_device() {
+    return (('ontouchstart' in window)
+    || (navigator.MaxTouchPoints > 0)
+    || (navigator.msMaxTouchPoints > 0));
+}
+
+
 export default class {
     static displayName = 'login-box';
     static template = template;
     static include = { iconUser }
+
+    static ready (el) {
+        if (is_touch_device()) {
+            var trigger = el.querySelector('a');
+            trigger.removeAttribute('href');
+            trigger.removeAttribute('target');
+        }
+    }
 }
